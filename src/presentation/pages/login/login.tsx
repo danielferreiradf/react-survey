f058ef0fddcb2336bb81ebd1aa4b75c3f05b4bc9
Daniel Ferreira da Silva <daniel.ferreira@zencargo.com>
2020-10-04 11:14:26 +0100

    feat: ensure Login presents error if email is invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -15,12 +15,16 @@ const Login: FC<Props> = (props: Props) => {
     isLoading: false,
     email: '',
     password: '',
-    emailError: 'Campo obrigatório',
+    emailError: '',
     passwordError: 'Campo obrigatório',
     mainError: ''
   })
 
   useEffect(() => {
+    setState({
+      ...state,
+      emailError: validation.validate('email', state.email)
+    })
     validation.validate('email', state.email)
   }, [state.email])
   useEffect(() => {
