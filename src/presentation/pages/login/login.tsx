382fd2048ade64631fef64c278fe114dde5b320e
Daniel Ferreira da Silva <daniel.ferreira@zencargo.com>
2020-10-05 20:09:13 +0100

    feat: ensure Login adds accessToken to local storage on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -38,10 +38,11 @@ const Login: FC<Props> = (props: Props) => {
         return
       }
       setState({ ...state, isLoading: true })
-      await authentication.auth({
+      const account = await authentication.auth({
         email: state.email,
         password: state.password
       })
+      localStorage.setItem('accessToken', account.accessToken)
     } catch (error) {
       setState({
         ...state,
