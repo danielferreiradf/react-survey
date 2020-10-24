f87996dee711a4a9e3e5e19d9d12560d632e4673
Daniel Ferreira da Silva <danielferreiradk@gmail.com>
2020-10-24 18:48:02 +0100

    feat: ensure RemoteAddAccount calls HttpPostClient with correct URL

--- src/data/usecases/authentication/remote-authencation.ts
+++ src/data/usecases/authentication/remote-authencation.ts
@@ -4,10 +4,16 @@ import { AccountModel } from '@/domain/models'
 import { Authentication, AuthenticationParams } from '@/domain/usecases'
 
 export class RemoteAuthentication implements Authentication {
-  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>) { }
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
+  ) { }
 
   async auth (params: AuthenticationParams): Promise<AccountModel> {
-    const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: return httpResponse.body
