e5bf91c5bf06d5735de08886135d38b61ecd9167
Daniel Ferreira da Silva <danielferreiradk@gmail.com>
2020-10-24 10:39:37 +0100

    feat: ensure Login calls SaveAccessToken with correct value

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,17 +3,18 @@ import Styles from './login-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
-import { Authentication } from '@/domain/usecases'
+import { Authentication, SaveAccessToken } from '@/domain/usecases'
 import { Link, useHistory } from 'react-router-dom'
 
 type Props = {
   validation: Validation
   authentication: Authentication
+  saveAccessToken: SaveAccessToken
 }
 
 const Login: FC<Props> = (props: Props) => {
+  const { validation, authentication, saveAccessToken } = props
   const history = useHistory()
-  const { validation, authentication } = props
 
   const [state, setState] = useState({
     isLoading: false,
@@ -43,7 +44,7 @@ const Login: FC<Props> = (props: Props) => {
         email: state.email,
         password: state.password
       })
-      localStorage.setItem('accessToken', account.accessToken)
+      await saveAccessToken.save(account.accessToken)
       history.replace('/')
     } catch (error) {
       setState({
